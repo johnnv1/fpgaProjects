288f9ad21c62174d4b587452295ca203d0bcb7f4
João Amorim <joaogustavoamorim@gmail.com>
2019-12-07 18:03:04 -0300

    update sim, add new test case

--- 12-hypotenuseWithBIST/sources/simulation/sim_hypotenuse.vhd
+++ 12-hypotenuseWithBIST/sources/simulation/sim_hypotenuse.vhd
@@ -21,6 +21,8 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.all;
+use IEEE.MATH_REAL.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -67,11 +69,12 @@ architecture rtl of sim_hypotenuse is
     signal test_B_sqr       :  STD_LOGIC_VECTOR (7 downto 0)    := "00001001"; -- 9 
     signal test_sum         :  STD_LOGIC_VECTOR (7 downto 0)    := "00011001"; -- 25       
     signal test_sqrt        :  STD_LOGIC_VECTOR (3 downto 0)    := "0101"; -- 5
-    signal sqr_a_res  :  STD_LOGIC_VECTOR (7 downto 0);
-    signal sqr_b_res  :  STD_LOGIC_VECTOR (7 downto 0);
-    signal sum_res      :  STD_LOGIC_VECTOR (7 downto 0);
-    signal sqrt_res      :  STD_LOGIC_VECTOR (3 downto 0);
-
+    signal sqr_a_res        :  STD_LOGIC_VECTOR (7 downto 0);
+    signal sqr_b_res        :  STD_LOGIC_VECTOR (7 downto 0);
+    signal sum_res          :  STD_LOGIC_VECTOR (7 downto 0);
+    signal sqrt_res         :  STD_LOGIC_VECTOR (3 downto 0);
+    
+    signal cont             :  STD_LOGIC_VECTOR (15 downto 0);
 begin
     
     clk <= not clk after 5 ns;
@@ -96,4 +99,38 @@ begin
         sum_res     => sum_res,
         sqrt_res    => sqrt_res
     );
+    
+    
+    process(clk)
+    begin
+    if(clk'event and clk='1') then
+        if(rst_n='1') then
+            cont <= cont + 1;
+            if (cont = 20) then
+                a_i             <= "0001"; -- 1
+                b_i             <= "0011"; -- 3
+                test_A_sqr      <= "00000001"; -- 1
+                test_B_sqr      <= "00001001"; -- 9 
+                test_sum        <= "00001010"; -- 10       
+                test_sqrt       <= "0011"; -- 3
+            elsif (cont = 40) then
+                a_i             <= "0101"; -- 5
+                b_i             <= "0010"; -- 2
+                test_A_sqr      <= "00011001"; -- 25
+                test_B_sqr      <= "00000100"; -- 4 
+                test_sum        <= "00011101"; -- 29       
+                test_sqrt       <= "1001"; -- 9 ou 10
+            elsif (cont = 60) then
+                a_i             <= "0100"; -- 4
+                b_i             <= "0011"; -- 3
+                test_A_sqr      <= "00001111"; -- 16 --- adicionado uma comparação errada aqui ou seja ta testando = 15
+                test_B_sqr      <= "00001001"; -- 9 
+                test_sum        <= "00011001"; -- 25       
+                test_sqrt       <= "0101"; -- 5
+            end if;
+        else
+             cont <= (others => '0');
+        end if;            
+    end if;
+    end process;
 end rtl;
